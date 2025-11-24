9be21bcef49eeabaac5dbe12c46f5d3a9788b46d
Denis Omerovic <denis.omerovic24@gmail.com>
2025-11-24 12:00:20 +0100

    Added Light/Dark Theme, some style improvements, added lightbox

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,30 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "alpine-turbo-drive-adapter"
+import Alpine from "alpinejs"
+
+window.Alpine = Alpine;
+
+document.addEventListener("alpine:init", () => {
+	Alpine.data("themeSwitcher", () => ({
+		theme: "light",
+		menuOpen: false,
+
+		init() {
+			const storedTheme = window.localStorage.getItem("theme");
+			this.theme = storedTheme || "light";
+		},
+
+		setTheme(name) {
+			this.theme = name;
+			window.localStorage.setItem("theme", name)
+		},
+
+		isActive(name) {
+			return this.theme === name;
+		}
+	}))
+})
+
+Alpine.start();
